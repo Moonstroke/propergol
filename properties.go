089f028fe0c4b4699e89fbbde778acedf7ce5603
Joachim MARIE <moonstroke+github@live.fr>
2025-05-04 00:43:38 +0200

    Add documentation blocks before all declarations
    The documentation paragraphs are terse and to-the-point. I still need to
    make sure that it conforms to conventional Go documentation authoring.

--- properties.go
+++ properties.go
@@ -4,22 +4,39 @@ import (
 	"bufio"
 )
 
+/*
+ * This structure represents a mapping of keys to values.
+ * It is intended to be used to centralize configuration data of an application.
+ * The property keys and values are represented as string objects.
+ */
 type Properties struct {
 	// TODO
 }
 
+/*
+ * Assign the given value to the property with the specified key.
+ */
 func (p *Properties) Set(key string, value string) {
 	// TODO
 }
 
+/*
+ * Retrieve the value of the property with the specified key.
+ */
 func (p *Properties) Get(key string) string {
 	return "" // TODO
 }
 
+/*
+ * Load properties in texte form from the given reader.
+ */
 func (p *Properties) Load(bufio.Reader) error {
 	return nil // TODO
 }
 
+/*
+ * Output the properties in text form to the given writer.
+ */
 func (p *Properties) Write(bufio.Writer) error {
 	return nil // TODO
 }
